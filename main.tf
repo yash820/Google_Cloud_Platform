d5781226519a190fae01707282846cebf9f6b96d
yash <kumar.yash476@gmail.com>
2025-06-23 19:29:12 +0530

    formaating main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "terraform-cicd-pipeline-bucket"
-  location      = "US"
-  force_destroy = true
-  project = "terraform-gcp-462915"
+  name                     = "terraform-cicd-pipeline-bucket"
+  location                 = "US"
+  force_destroy            = true
+  project                  = "terraform-gcp-462915"
   public_access_prevention = "enforced"
 }
