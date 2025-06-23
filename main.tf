9c4b80ae0bc480c4c776e9537d57fcd5a32a361f
yash <kumar.yash476@gmail.com>
2025-06-23 19:27:23 +0530

    Create bucket in GCP

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "my-bucket" {
   name          = "terraform-cicd-pipeline-bucket"
   location      = "US"
   force_destroy = true
-
+  project = "terraform-gcp-462915"
   public_access_prevention = "enforced"
 }
