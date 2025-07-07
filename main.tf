1a259691ac3f7dd2de242dbbe3c6a297df35bc0a
yash <kumar.yash476@gmail.com>
2025-07-07 13:17:51 +0530

    create sixth cuket

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-resource "google_storage_bucket" "my-bucket" {
+resource "google_storage_bucket" "my-bucket1" {
   name                     = "terraform-cicd-pipeline-bucket"
+  location                 = "US"
+  force_destroy            = true
+  project                  = "terraform-gcp-462915"
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "my_sixth_bucket_using_workflow"
   location                 = "US"
   force_destroy            = true
   project                  = "terraform-gcp-462915"
