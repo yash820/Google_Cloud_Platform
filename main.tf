afde4590cdb54bdd21bfb1e5aba87c179f8a16e7
Yash Kumar Singh <77056645+yash820@users.noreply.github.com>
2025-07-17 16:26:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 
 resource "google_storage_bucket" "my-bucket1" {
-  name                     = "my_sixth_bucket_using_workflow"
+  name                     = "my_eighth_bucket_self_hosted_runner"
   location                 = "US"
   force_destroy            = true
   project                  = "terraform-gcp-462915"
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "my_seventh_bucket_using_workflow"
-  location                 = "US"
-  force_destroy            = true
-  project                  = "terraform-gcp-462915"
-  public_access_prevention = "enforced"
-}
